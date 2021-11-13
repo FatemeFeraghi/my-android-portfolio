d1706ac8c5d01e3eb85e0203d28bea730c4ea263
Fateme Feraghi <fatemeferaghi@Fatemes-MacBook-Pro.local>
2021-11-13 16:41:21 -0500

    Change the quiz app project image

--- app/src/main/java/com/fateme/fatemeandroidportfolio/MainActivity.java
+++ app/src/main/java/com/fateme/fatemeandroidportfolio/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView list = findViewById(R.id.recycler_view_projects);
 
         Project[] projects = {
-                new Project("Quiz App","A quiz app, teaching me about Activities, Listeners, Objects, ArrayLists and Intents.",R.drawable.restau),
+                new Project("Quiz App","A quiz app, teaching me about Activities, Listeners, Objects, ArrayLists and Intents.",R.drawable.quiz),
                 new Project("BMI Calculator","A BMI calculator, using conditional statements, variables, ListViews and objects.",R.drawable.weight2),
                 new Project("Sport App","An sport multi screen app, learning about Android Layouts and Views, performing clear, next and quit Buttons.",R.drawable.sport),
                 new Project("Restaurant App","A menu app for a restaurant, learning about Activities, Classes, Objects, Arrays, Intents and Listviews.",R.drawable.restau),
