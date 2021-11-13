eb0513d45a24bda08b0bdee0b134a29e6d5d963a
Fateme Feraghi <fatemeferaghi@Fatemes-MacBook-Pro.local>
2021-11-13 16:07:32 -0500

    Add TicTocProject to the portfolio app.

--- app/src/main/java/com/fateme/fatemeandroidportfolio/MainActivity.java
+++ app/src/main/java/com/fateme/fatemeandroidportfolio/MainActivity.java
@@ -20,8 +20,10 @@ public class MainActivity extends AppCompatActivity {
                 new Project("BMI Calculator","A BMI calculator, using conditional statements, variables, ListViews and objects.",R.drawable.weight2),
                 new Project("Sport App","An sport multi screen app, learning about Android Layouts and Views, performing clear, next and quit Buttons.",R.drawable.sport),
                 new Project("Restaurant App","A menu app for a restaurant, learning about Activities, Classes, Objects, Arrays, Intents and Listviews.",R.drawable.restau),
+                new Project("TicTocGame","A game app, learning about TableLayout, ClickListeners, for loops and possible conditions in an XOX game when the user clicks on each cell.",R.drawable.tictocgame)
         };
 
+        
         ProjectsAdapter adapter = new ProjectsAdapter(projects);
         list.setAdapter(adapter);
 
